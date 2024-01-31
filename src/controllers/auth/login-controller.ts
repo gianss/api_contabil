5dd07fe043772b5466986e492bbfccc5aba15f94
gian <gian_ss@live.com>
2024-01-31 06:21:19 -0300

    feat: creating add controller

---
+++ src/controllers/auth/login-controller.ts
@@ -0,0 +1,6 @@
+import { LoginImplemetation } from './protocols/login-implemetation'
+
+export class LoginController implements LoginImplemetation {
+    async handle(request: any): Promise<any> {
+    }
+}
---
+++ src/controllers/auth/protocols/login-implemetation.ts
@@ -0,0 +1,3 @@
+export interface LoginImplemetation {
+    handle: (request: any) => any
+}
