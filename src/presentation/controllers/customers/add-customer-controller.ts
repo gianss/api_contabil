600366c20f0ed322f62a2ff7e5301a470f55a2e5
gian <gian_ss@live.com>
2024-02-02 09:49:53 -0300

    feat: adding validation to handle method

--- src/presentation/controllers/customers/add-customer-controller.ts
+++ src/presentation/controllers/customers/add-customer-controller.ts
@@ -1,7 +1,7 @@
 import { AddCustomerHandler } from '@/domain/usecases/customers/add-customer'
 import { AddCustomerService } from '@/domain/usecases/customers/customer-service'
 import { AddCustomerRequest } from '@/presentation/dtos/add-customer-request'
-import { ok } from '@/presentation/helpers/http-helper'
+import { badRequest, ok, serverError } from '@/presentation/helpers/http-helper'
 import { HttpResponse } from '@/presentation/http/http-response'
 import { Validation } from '@/validation/protocols'
 
@@ -12,6 +12,14 @@ export class AddCustomerController implements AddCustomerHandler {
     ) { }
 
     async handle(request: AddCustomerRequest): Promise<HttpResponse> {
-        return ok('ok')
+        try {
+            const error = this.validation.validate(request)
+            if (error) {
+                return badRequest(error)
+            }
+            return ok('ok')
+        } catch (error) {
+            return serverError(error)
+        }
     }
 }
