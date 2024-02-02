5749065a466767e619d2b2d9dba42722a3df0c7c
gian <gian_ss@live.com>
2024-02-02 11:58:33 -0300

    fix: correcting import

--- src/infra/hasher/jwt-adapter.ts
+++ src/infra/hasher/jwt-adapter.ts
@@ -1,8 +1,8 @@
+import { JwtHashGenerator } from '@/domain/usecases/auth'
 import config from '@/infra/config/config'
-import { JwtAdapterInterface } from '@/domain/usecases/auth'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements JwtAdapterInterface {
+export class JwtAdapter implements JwtHashGenerator {
     async generateHash(item: any): Promise<string> {
         return jwt.sign({ item }, config.jwt_key, { expiresIn: 3600 })
     }
