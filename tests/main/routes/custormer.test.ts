c5f1b74a67c420bcf92ff07b1f73148bfb35be94
gian <gian_ss@live.com>
2024-02-03 08:34:14 -0300

    test: should return 401 if not allowed

--- tests/main/routes/custormer.test.ts
+++ tests/main/routes/custormer.test.ts
@@ -3,11 +3,17 @@ import server from '@/main/index'
 import { faker } from '@faker-js/faker'
 
 let authToken: string
+let authTokenNotAllowed: string
 beforeAll(async () => {
     const loginResponse = await request(server)
         .post('/auth/login')
         .send({ email: 'gian_ss@live.com', password: '123' })
     authToken = loginResponse.body.token
+
+    const loginResponseNotAllowed = await request(server)
+        .post('/auth/login')
+        .send({ email: 'igorfals@gmail.com', password: '123' })
+    authTokenNotAllowed = loginResponseNotAllowed.body.token
 })
 
 afterAll((done) => {
@@ -36,6 +42,14 @@ describe('Customer Endpoint Tests', () => {
         const response = await request(server)
             .post('/customer')
             .set('x-access-token', 'any_token')
+            .send({ name: faker.person.fullName(), email: faker.internet.email() })
+        expect(response.status).toBe(401)
+    })
+
+    test('should return 401 if not allowed', async () => {
+        const response = await request(server)
+            .post('/customer')
+            .set('x-access-token', authTokenNotAllowed)
             .send({ name: faker.person.fullName(), email: faker.internet.email() })
         expect(response.status).toBe(401)
     })
