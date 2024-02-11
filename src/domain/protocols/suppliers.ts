4ffb08359344c6922e74a44c1af2e1f70d752784
gian <gian_ss@live.com>
2024-02-10 21:48:58 -0300

    feat: add suppliers protocol

---
+++ src/domain/protocols/suppliers.ts
@@ -0,0 +1,10 @@
+export interface Suppliers {
+    id: number
+    name: string
+    phone: string
+    email: string
+    status: string
+    company_id: number
+    created_at: string | number | Date
+    updated_at: string | number | Date
+}
